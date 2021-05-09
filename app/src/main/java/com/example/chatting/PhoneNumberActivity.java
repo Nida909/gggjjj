0fa959a25288ca2f5354e9c24ea2ebf7ae5a3394
Laiba098 <laibaabbasi098@gmail.com>
2021-05-09 08:04:27 +0500

    fyp

--- app/src/main/java/com/example/chatting/PhoneNumberActivity.java
+++ app/src/main/java/com/example/chatting/PhoneNumberActivity.java
@@ -1,10 +1,13 @@
 package com.example.chatting;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,23 +18,62 @@ public class PhoneNumberActivity extends AppCompatActivity {
     ActivityPhoneNumberBinding binding;
     Button continueBtn;
     EditText phoneBox;
-    String str;
+    String str,str1;
     FirebaseAuth auth;
+    String languages;
+    TextView verify,id1;
+    Context context;
+    Resources resources;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        binding.phoneBox.requestFocus();
-        continueBtn=findViewById(R.id.continueBtn);
-        phoneBox=findViewById(R.id.phoneBox);
-         auth=FirebaseAuth.getInstance();
-        getSupportActionBar().hide();
+        verify=findViewById(R.id.verify);
+        id1=findViewById(R.id.id1);
+        Intent inten=getIntent();
+        languages = inten.getExtras().getString("language");
+        if(languages.equals("اردو")) {
+
+            context = LocalHelper.setLocale(PhoneNumberActivity.this, "an");
+            resources = context.getResources();
+            verify.setText(resources.getString(R.string.verify));
+            id1.setText(resources.getString(R.string.otp));
+            phoneBox.setText(resources.getString(R.string.phonebox));
+            continueBtn.setText(resources.getString(R.string.continuebtn));
+            binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+            binding.phoneBox.requestFocus();
+            continueBtn = findViewById(R.id.continueBtn);
+            phoneBox = findViewById(R.id.phoneBox);
+            auth = FirebaseAuth.getInstance();
+            getSupportActionBar().hide();
+            str1="اردو";
+
+
+        }
+        if(languages.equals("ENGLISH")) {
+
+            context = LocalHelper.setLocale(PhoneNumberActivity.this, "en");
+            resources = context.getResources();
+            verify.setText(resources.getString(R.string.verify));
+            id1.setText(resources.getString(R.string.otp));
+            phoneBox.setText(resources.getString(R.string.phonebox));
+            continueBtn.setText(resources.getString(R.string.continuebtn));
+            binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+            binding.phoneBox.requestFocus();
+            continueBtn = findViewById(R.id.continueBtn);
+            phoneBox = findViewById(R.id.phoneBox);
+            auth = FirebaseAuth.getInstance();
+            getSupportActionBar().hide();
+            str1="ENGLISH";
+
+        }
        if(auth.getCurrentUser()!=null)
         {
             Intent intent=new Intent(PhoneNumberActivity.this, MainActivity.class);
+            inten.putExtra("language",str1);
             startActivity(intent);
             finish();
         }
@@ -44,6 +86,7 @@ public class PhoneNumberActivity extends AppCompatActivity {
         str=phoneBox.getText().toString();
         Intent intent=new Intent(this,OTP.class);
         intent.putExtra("val",str);
+        intent.putExtra("language",str1);
         startActivity(intent);
 
 
